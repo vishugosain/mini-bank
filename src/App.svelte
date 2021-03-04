b1657d71391f120138c2f36bcb0281aa36892fb1
vishalgosain <gosain.vishal437@gmail.com>
2021-03-05 00:56:00 +0530

    Added Github Link

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,19 @@
 	import Router, {push} from 'svelte-spa-router';
 	import {routes} from './router';
 
+	let profileLinks = [
+		{
+			link: 'https://github.com/vishugosain/mini-bank',
+			icon: 'assets/github.svg',
+			altText: 'Github'
+		},
+		{
+			link: 'https://svelte.dev/',
+			icon: 'assets/svelte.svg',
+			altText: 'Svelte'
+		}
+	]
+
 	function deleteAll() {
 		wallet.reset();
 		push('/');
@@ -10,6 +23,11 @@
 </script>
 
 <main>
+	<div class="about-wrap">
+		{#each profileLinks as link}
+			<a href={link.link}><img src={link.icon} alt={link.altText}></a>
+		{/each}
+	</div>
 	<img src="assets/bank.svg" alt="MiniBank">
 	<h1>Mini Bank</h1>
 	<p>Store all your transactions</p>
@@ -30,6 +48,20 @@
 		justify-content: center;
 		flex-direction: column;
 		overflow: auto;
+		.about-wrap {
+			position: fixed;
+			top: 0;
+			right: 0;
+			padding: 16px;
+			display: flex;
+			flex-direction: column;
+			img {
+				height: 36px;
+				width: 36px;
+				margin: 4px 0;
+				object-fit: contain;
+			}
+		}
 		.top-bar {
 			padding-top: 16px;
         }
